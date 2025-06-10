d5f0fb3c62095ade01c299e4fd5c177176ba7117
MoltenCrystal <moltencrystal_dev@outlook.com>
2025-06-10 21:53:15 +0200

    Attempt to fix banner

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -130,15 +130,15 @@ extern int main(int argc, char** argv)
 
     TC_LOG_INFO("server.authserver", "%s (authserver)", GitRevision::GetFullVersion());
     TC_LOG_INFO("server.authserver", "<Ctrl-C> to stop.\n");
-    TC_LOG_INFO("server.worldserver", "( ______                       __)");
-    TC_LOG_INFO("server.worldserver", "(/\__  _\       __          __/\ \__)");
-    TC_LOG_INFO("server.worldserver", "(\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __)");
-    TC_LOG_INFO("server.worldserver", "(   \ \ \/\`'__\/\ \ /' _ `\/\ \ \ \/ /\ \/\ \)");
-    TC_LOG_INFO("server.worldserver", "(    \ \ \ \ \/ \ \ \/\ \/\ \ \ \ \ \_\ \ \_\ \)");
-    TC_LOG_INFO("server.worldserver", "(     \ \_\ \_\  \ \_\ \_\ \_\ \_\ \__\\/`____ \)");
-    TC_LOG_INFO("server.worldserver", "(      \/_/\/_/   \/_/\/_/\/_/\/_/\/__/ `/___/> \)");
-    TC_LOG_INFO("server.worldserver", "(                                 C O R E  /\___/)");
-    TC_LOG_INFO("server.worldserver", "(http://TrinityCore.org                    \/__/)" "\n");
+    TC_LOG_INFO("server.worldserver", " ______                       __");
+    TC_LOG_INFO("server.worldserver", "/\__  _\       __          __/\ \__");
+    TC_LOG_INFO("server.worldserver", "\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __");
+    TC_LOG_INFO("server.worldserver", "   \ \ \/\`'__\/\ \ /' _ `\/\ \ \ \/ /\ \/\ \");
+    TC_LOG_INFO("server.worldserver", "    \ \ \ \ \/ \ \ \/\ \/\ \ \ \ \ \_\ \ \_\ \");
+    TC_LOG_INFO("server.worldserver", "     \ \_\ \_\  \ \_\ \_\ \_\ \_\ \__\\/`____ \");
+    TC_LOG_INFO("server.worldserver", "      \/_/\/_/   \/_/\/_/\/_/\/_/\/__/ `/___/> \");
+    TC_LOG_INFO("server.worldserver", "                                 C O R E  /\___/");
+    TC_LOG_INFO("server.worldserver", "http://TrinityCore.org                    \/__/" "\n");
 
     TC_LOG_INFO("server.authserver", "Using configuration file %s.", configFile);
 
--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -169,15 +169,15 @@ int Master::Run()
 
     TC_LOG_INFO("server.worldserver", "%s (worldserver-daemon)", GitRevision::GetFullVersion());
     TC_LOG_INFO("server.worldserver", "<Ctrl-C> to stop.\n");
-    TC_LOG_INFO("server.worldserver", "( ______                       __)");
-    TC_LOG_INFO("server.worldserver", "(/\__  _\       __          __/\ \__)");
-    TC_LOG_INFO("server.worldserver", "(\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __)");
-    TC_LOG_INFO("server.worldserver", "(   \ \ \/\`'__\/\ \ /' _ `\/\ \ \ \/ /\ \/\ \)");
-    TC_LOG_INFO("server.worldserver", "(    \ \ \ \ \/ \ \ \/\ \/\ \ \ \ \ \_\ \ \_\ \)");
-    TC_LOG_INFO("server.worldserver", "(     \ \_\ \_\  \ \_\ \_\ \_\ \_\ \__\\/`____ \)");
-    TC_LOG_INFO("server.worldserver", "(      \/_/\/_/   \/_/\/_/\/_/\/_/\/__/ `/___/> \)");
-    TC_LOG_INFO("server.worldserver", "(                                 C O R E  /\___/)");
-    TC_LOG_INFO("server.worldserver", "(http://TrinityCore.org                    \/__/)" "\n");
+    TC_LOG_INFO("server.worldserver", " ______                       __");
+    TC_LOG_INFO("server.worldserver", "/\__  _\       __          __/\ \__");
+    TC_LOG_INFO("server.worldserver", "\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __");
+    TC_LOG_INFO("server.worldserver", "   \ \ \/\`'__\/\ \ /' _ `\/\ \ \ \/ /\ \/\ \");
+    TC_LOG_INFO("server.worldserver", "    \ \ \ \ \/ \ \ \/\ \/\ \ \ \ \ \_\ \ \_\ \");
+    TC_LOG_INFO("server.worldserver", "     \ \_\ \_\  \ \_\ \_\ \_\ \_\ \__\\/`____ \");
+    TC_LOG_INFO("server.worldserver", "      \/_/\/_/   \/_/\/_/\/_/\/_/\/__/ `/___/> \");
+    TC_LOG_INFO("server.worldserver", "                                 C O R E  /\___/");
+    TC_LOG_INFO("server.worldserver", "http://TrinityCore.org                    \/__/" "\n");
 
 
     /// worldserver PID file creation
